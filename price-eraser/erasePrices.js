fde5175a50731f740483974ba92ee472e73d0541
Paweł Lal <pawellal@gmail.com>
2022-01-04 15:52:48 +0100

    Cleanup

--- price-eraser/erasePrices.js
+++ price-eraser/erasePrices.js
@@ -1,15 +1,3 @@
-/*var div = $('div[class="box-content-row"');
-var innerDiv = [];
-innerDiv = div.children("div");
-
-for (i = 0; i < innerDiv.length - 1; i++) {
-	var innerA = innerDiv.eq(i).children("a:first");
-	console.log(innerA);
-	var url = innerA.attr("href");
-	console.log(url);
-	window.open(url, '_blank');
-}
-*/
 document.documentElement.style.visibility = 'hidden';
 document.addEventListener('DOMContentLoaded', function() {	
 	var prices = document.getElementsByClassName('price');
